b837232a01f57cef47b77d3407ff82ec9e0ee197
arzakmiftakhul <miftakhularzak@gmail.com>
2019-05-21 01:58:22 +0700

    Update create-table.sql

--- create-table.sql
+++ create-table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Pendaftaran(
-   ID   INT              NOT NULL,
+   ID INT NOT NULL IDENTITY(1,1) PRIMARY KEY(id),
    NAME VARCHAR (20)     NOT NULL,
    EMAIL VARCHAR (25)     NOT NULL,
    NOHP INT                NOT NULL,

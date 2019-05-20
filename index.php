601bc659079aabec23c122bf36f5d60d830ca11e
arzakmiftakhul <miftakhularzak@gmail.com>
2019-05-21 01:35:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -68,7 +68,8 @@
                 echo "<table>";
                 echo "<tr><th>Name</th>";
                 echo "<th>Email</th>";
-                echo "<th>Job</th>";
+		echo "<th>No Hp</th>";
+                echo "<th>Pekerjaan</th>";
                 echo "<th>Date</th></tr>";
                 foreach($registrants as $registrant) {
                     echo "<tr><td>".$registrant['name']."</td>";
